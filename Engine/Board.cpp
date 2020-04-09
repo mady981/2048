e94c8257ed47ee96a7b2642937f75e4194e84646
mady981 <moritz-eibl@web.de>
2020-04-09 19:33:23 +0200

    started rewriting board

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -4,27 +4,7 @@ Board::Board( std::random_device& rng )
 	:
 	rng( rng )
 {
-	for ( int y = 0; y < Height; ++y )
-	{
-		for ( int x = 0; x < Width; ++x )
-		{
-			Tiles[y * Width + x] = RecI( x * TileSize,( x + 1 ) * TileSize,y * TileSize,( y + 1 ) * TileSize);
-		}
-	}
-}
-
-void Board::MoveTiles( const Vec2i& dir )
-{
-}
-
-void Board::Draw( Graphics& gfx ) const
-{
-	for ( int i = 0; i < ( Width * Height ); ++i )
-	{
-		gfx.DrawRec( Tiles[i],Colors::Gray );
-	}
-}
-
-void Board::SpawnTile( std::mt19937& rng )
-{
+	std::uniform_int_distribution<int> xDist( 0,width );
+	std::uniform_int_distribution<int> yDist( 0,height );
+	tiles.push_back( tiles[1].setpos( Vec2i( xDist( rng ),yDist( rng ) ) ) );
 }
--- Engine/Board.h
+++ Engine/Board.h
@@ -1,21 +1,36 @@
 #pragma once
 #include "Graphics.h"
 #include "Vec2.h"
+#include <vector>
 #include <random>
 
 class Board
 {
+private:
+	class Tile
+	{
+	public:
+		Tile( const Vec2i& pos );
+		void Move();
+		void Draw();
+		Vec2i getpos() const;
+		Vec2i setpos( const Vec2i& val );
+	private:
+		Vec2_<int> pos;
+	};
 public:
 	Board( std::random_device& rng );
-	void MoveTiles( const Vec2i& dir );
-	void Draw( Graphics& gfx ) const;
+	void Move();
+	void Draw();
 private:
-	void SpawnTile( std::mt19937& rng );
+	void AddTile();
 private:
 	static constexpr int TileSize = 128;
-	static constexpr int Width = 4;
-	static constexpr int Height = 4;
+	static constexpr int width = 4;
+	static constexpr int height = 4;
+	int nTiles = 2;
 private:
 	std::random_device& rng;
-	RecI Tiles[Width * Height];
+	std::vector<Tile> tiles;
+	bool istile[width * height] = { false };
 };
